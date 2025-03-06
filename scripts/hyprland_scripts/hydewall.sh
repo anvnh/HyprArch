be57c1c15363fc4a15e51cd27f6ab2c840b4de9b
anvnh <anvo20052@gmail.com>
2025-03-06 15:22:55 +0700

    Testing symlink

--- scripts/hyprland_scripts/hydewall.sh
+++
@@ -1,7 +0,0 @@
-source ./scripts/message.sh
-
-# Copy content of hydewall to .cache/hyde/
-mkdir -p ~/.cache/hyde/
-cp -r ./configs/hydewall/wall.blur ~/.cache/hyde/
-cp -r ./configs/hydewall/wall.thmb ~/.cache/hyde/
-
